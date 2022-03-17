79510c84a7dca16300db27590ed889ecc7daeea3
suzanadeazevedo <suzanadeazevedo@hotmail.com>
2022-03-17 20:54:47 -0300

    1.3

---
+++ Aula 04 - Módulo e Bibliotecas/aula04.py
@@ -0,0 +1,7 @@
+# import  math
+from math import sqrt
+
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+# print('A raíz de {} é igual a {}'.format(num, math.floor(raiz)))
+print('A raíz de {} é igual a {}'.format(num, raiz))
