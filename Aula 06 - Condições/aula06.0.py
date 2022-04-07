3ebbff6dba601c9a72d07d971ca1020d2e940229
suzanadeazevedo <suzanadeazevedo@hotmail.com>
2022-04-07 19:15:37 -0300

    1.6

---
+++ Aula 06 - Condições/aula06.0.py
@@ -0,0 +1,12 @@
+#if condicao():
+#else:
+time = int(input('Quantos anos tem seu carro?'))
+print('Carro Novo' if time<=3 else 'Carro Velho')
+print('--Fim--')
+
+tempo = int(input('Digite quantos anos o seu carro tem: '))
+if tempo <=3:
+    print('Carro Novo')
+else:
+    print('Carro Velho')
+print('--FIM--')
